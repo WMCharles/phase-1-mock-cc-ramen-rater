2ae97d85969130e339f2cccf362e00c3997e6ffe
WMCharles <charles.wafula@student.moringaschool.com>
2022-08-28 09:06:31 +0300

    add js fuctions to fetch and display data from json file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 // write your code here
+//Fetching data from backend
+function fetchRamen(){
+    fetch("http://localhost:3000/ramens")
+    .then(resp => resp.json())
+    .then(ramen => ramen.forEach(dish => renderDish(dish)))
+}
+//Displaying data from db
+function renderDish(dish){
+    let rdish = document.createElement('img')
+    rdish.src = dish.image
+    document.getElementById('ramen-menu').appendChild(rdish)
+}
+
+function displaDish(){
+    fetchRamen()
+}
+
+displaDish()
